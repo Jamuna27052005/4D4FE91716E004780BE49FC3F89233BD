8f05a5e7ec6862f557bd1ba1a4113a21c0d7195e
Jamuna27052005 <144685128+Jamuna27052005@users.noreply.github.com>
2023-09-29 18:36:59 +0530

    Add files via upload

---
+++ main (2).py
@@ -0,0 +1,17 @@
+Class Bank_Account:
+    def_int_(self):
+       Self.balance=C 
+       printf("Hello!!! Welcome to the Deposit & Withdrawal Machine")
+
+def deposit(self):
+     amount=float(input("Enter amount to be Deposited:"))
+       Self.balance+=amount 
+       print("\nAmount Deposited:", amount)
+
+def withdraw(self)
+     amount=float(input("Enter amount to be withdrew:"))
+     if Self.balance>=amount :
+          Self.balance =amount 
+          Printf("\n you withdrew:", amount)
+else:
+         Printf("\n Insufficient balance")
