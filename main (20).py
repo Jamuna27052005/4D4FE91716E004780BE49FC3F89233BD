5d70df1f879045b2f3978365cabbe18335d62331
Jamuna27052005 <144685128+Jamuna27052005@users.noreply.github.com>
2023-10-16 17:56:35 +0530

    Add files via upload

---
+++ main (20).py
@@ -0,0 +1,16 @@
+def 
+linear search product(product list, target product)
+indices=1
+
+for index, product in enumerate(product list)
+if product ==target product 
+indices.append(index)
+
+return indices
+#Example usage:
+product=["shoes","boot","loafer", "shoes","sandal","shoes"]
+target ="shoes"
+target 2='apple'
+result=
+linear search product (product, target)
+Print(result)
